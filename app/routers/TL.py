d67d2af702dc8c50628af700e27b967d10e7f888
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-07-21 14:04:49 +0200

    Download from TL implemented without version
    Can give pkg_id and it finds the url, scrapes all the links to download individual files, downloads them and adds to zip, returns zip
    
    Has problems if page on TL has folders
    
    To download right version, I need json file that maps version to commit hash

--- app/routers/TL.py
+++ app/routers/TL.py
@@ -1,8 +1,14 @@
+import io
 import logging
 
 from datetime import date
-from fastapi import HTTPException, status
+import os
+from os.path import join
+from urllib.parse import urljoin
+import zipfile
+from fastapi import HTTPException, Response, status
 import requests
+from bs4 import BeautifulSoup as bs4
 
 from app.schemas import Package, Version
 
@@ -10,7 +16,52 @@ logger = logging.getLogger("default")
 
 def download_pkg(pkg: Package, version: Version):
     print(f"TL: Downloading {pkg.id} {version}")
+    """
+        let elems = document.querySelectorAll("div.content td > a.ls-blob")
+        let links = Array.from(elems).map(elem => elem.getAttribute("href"))
+    """
+    if not pkg.ctan and pkg.ctan.path:
+        raise NotImplementedError("Can only download packages where I know the ctan path")
     
+    base_url = "https://git.texlive.info/CTAN/plain"
+    url = base_url + pkg.ctan.path
+    page = requests.get(url)  
 
-    
-    # return response.content
+    soup = bs4(page.content, "html.parser")
+
+    a_tags = soup.select("ul a")
+    urls = [urljoin(base_url, elem['href']) for elem in a_tags if elem.text != "../"]
+
+    print(urls)
+
+    return write_files_to_binary_zip(urls, pkg.id)
+
+
+def write_files_to_binary_zip(file_urls: list[str], pkg_id: str):
+    zip_filename = "%s.zip" % pkg_id
+
+    s = io.BytesIO()
+    zf = zipfile.ZipFile(file=s, mode="w")
+
+    for url in file_urls:
+        # Calculate path for file in zip
+        fname = os.path.basename(url)
+        if not fname: # E.g. hyperref, which has /doc folder
+            continue
+
+        resp = requests.get(url)
+        if not resp.ok:
+            raise RuntimeError("Couldnt get file at " + url)
+        
+        # Add file, at correct path
+        zf.writestr(data=resp.content, zinfo_or_arcname = fname)
+
+    # Must close zip for all contents to be written
+    zf.close()
+
+    # Grab ZIP file from in-memory, make response with correct MIME-type
+    resp = Response(s.getvalue(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={zip_filename}'
+    })
+
+    return resp
--- app/routers/packages.py
+++ app/routers/packages.py
@@ -25,21 +25,25 @@ def get_package(ctan_pkg: Package = Depends(pkg_id_exists), date: Union[date, No
     req_version = Version(number=number, date=date)
      
      # If version = latest or requested version equal to version on CTAN: Download from CTAN
-    if not (date or number) or check_satisfying(ctan_pkg.version, req_version):
+    if check_satisfying(ctan_pkg.version, req_version):
         return StreamingResponse(io.BytesIO(CTAN.download_pkg(ctan_pkg)))
 
     return TL.download_pkg(ctan_pkg, req_version)
 
 
-def check_satisfying(ctan: Version, req: Version):
-    if not isinstance (ctan.date, date):
-        if type(ctan.date) == str:
-            ctan.date = valid_date(ctan.date)
-    if not isinstance (req.date, date):
-        if type(req.date) == str:
-            req.date = valid_date(req.date)
+def check_satisfying(ctan_version: Version, req_version: Version):
+    if not (req_version.date or req_version.number): # Call doesn't specify version
+        return True
+    if ctan_version == None: # CTAN has no version, but call wants specific version
+        return False
+    # if not isinstance (ctan_version.date, date):
+    #     if type(ctan_version.date) == str:
+    #         ctan_version.date = valid_date(ctan_version.date)
+    # if not isinstance (req_version.date, date):
+    #     if type(req_version.date) == str:
+    #         req_version.date = valid_date(req_version.date)
     
     # TODO: Could break up more to give the reason why they're not equal
-    if ctan.date == req.date and ctan.number == req.number:
+    if ctan_version.date == req_version.date and ctan_version.number == req_version.number:
         return True
     return False
