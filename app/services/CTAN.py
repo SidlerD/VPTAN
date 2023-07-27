6c7650c2b63de2c1ccb51f91c6fbf2b1e43d6fe8
root <Dominik.sidler00@gmail.com>
2023-07-27 21:44:34 +0200

    Download works for single files now
    Seems like for some packages, CTAN doesn't return a path to a folder but rather a single file, e.g. accenti

--- app/services/CTAN.py
+++ app/services/CTAN.py
@@ -3,6 +3,7 @@ import logging
 from datetime import date
 from fastapi import HTTPException, status
 import requests
+from app.helpers import helpers
 
 from app.schemas import Package
 
@@ -18,14 +19,19 @@ def download_pkg(pkg: Package) -> bytes:
     
     elif pkg.ctan:
         path = pkg.ctan.path
-        url = f"https://mirror.ctan.org/tex-archive/{path}.zip"
+        url = f"https://mirror.ctan.org/tex-archive/{path}{'' if '.' in path else '.zip' }"
     else:
         if pkg.id:
             raise HTTPException(status_code=400, detail={'reason': f"{pkg.id} is not downloadable", 'CTAN_response': pkg})
         raise HTTPException(status_code=400, detail={'reason': f"{pkg.id} not available on CTAN", 'CTAN_response': pkg})
+    
+    if url.endswith('.zip'):
+        response = requests.get(url, allow_redirects=True)
+        if not response.ok:
+            raise HTTPException(400, response.reason)
 
-    response = requests.get(url, allow_redirects=True)
-    if not response.ok:
-        raise HTTPException(400, response.reason)
+        return response.content
+    else:
+        return helpers.download_files_to_binary_zip([url], pkg.id)
 
-    return response.content
+    
